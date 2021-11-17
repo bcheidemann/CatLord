071832837a1cf2898457d705a538c667b26a9842
bcheidemann <ben@heidemann.co.uk>
2021-11-17 01:04:42 +0000

    wip1

--- libs/cms/README.md
+++
@@ -1,7 +0,0 @@
-# cdn
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test cdn` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/cms/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'cdn',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/cdn',
-};
