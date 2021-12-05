c11397cd509a71ed03e8404d08032d99be679acb
bcheidemann <ben@heidemann.co.uk>
2021-12-05 11:43:05 +0000

    add news articles to CMS

--- apps/cms/schemas/schema.js
+++ apps/cms/schemas/schema.js
@@ -37,5 +37,49 @@ export default createSchema({
         },
       ]
     },
+    {
+      name: 'article',
+      type: 'document',
+      title: 'Article',
+      fields: [
+        {
+          title: 'Name',
+          name: 'name',
+          type: 'string',
+        },
+        {
+          title: 'Author',
+          name: 'author',
+          type: 'string',
+        },
+        {
+          title: 'Tags',
+          name: 'tags',
+          type: 'array',
+          of: [
+            { type: 'string' },
+          ],
+        },
+        {
+          title: 'ID',
+          name: 'id',
+          type: 'string',
+        },
+        {
+          title: 'Publish',
+          name: 'publishAt',
+          type: 'datetime'
+        },
+        {
+          title: 'Content',
+          name: 'content',
+          type: 'array',
+          of: [
+            { type: 'block' },
+            { type: 'image' },
+          ],
+        },
+      ]
+    },
   ]),
 })
